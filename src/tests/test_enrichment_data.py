e25307dd80897426b022d4b4a5a53c8cba04e2c5
Harin <harin.lee@cellsignal.com>
2024-11-13 13:40:25 -0500

    updated tests using 1.0.0 kl api

--- src/tests/test_enrichment_data.py
+++ src/tests/test_enrichment_data.py
@@ -6,11 +6,10 @@ def test_enrichment_ser_thr(
         enrichment_foreground_ser_thr_input_path, enrichment_background_ser_thr_input_path, enrichment_ser_thr_output_path):
     input_foreground_df = pd.read_csv(enrichment_foreground_ser_thr_input_path)
     input_background_df = pd.read_csv(enrichment_background_ser_thr_input_path)
-    enrichment_object = kl.phosphoproteomics.EnrichmentData(
-        input_foreground_df, input_background_df, kin_type="ser_thr")
-    output_ser_thr = enrichment_object.kinase_enrichment(non_canonical=False,
+    enrichment_object = kl.EnrichmentData(
+        input_foreground_df, input_background_df, fg_seq_col="SITE_+/-7_AA", bg_seq_col="SITE_+/-7_AA",)
+    output_ser_thr = enrichment_object.kinase_enrichment(non_canonical=False, kin_type="ser_thr",
                                                          kl_method="percentile_rank", kl_thresh=15, kinases=['AKT1', 'AKT2'])
-
     expected_df = pd.read_csv(enrichment_ser_thr_output_path, index_col=0)
     pd.testing.assert_frame_equal(output_ser_thr.enrichment_results, expected_df)
 
@@ -19,10 +18,9 @@ def test_enrichment_tyrosine(
         enrichment_foreground_tyrosine_input_path, enrichment_background_tyrosine_input_path, enrichment_tyrosine_output_path):
     input_foreground_df = pd.read_csv(enrichment_foreground_tyrosine_input_path)
     input_background_df = pd.read_csv(enrichment_background_tyrosine_input_path)
-    enrichment_object = kl.phosphoproteomics.EnrichmentData(
-        input_foreground_df, input_background_df, kin_type="tyrosine")
-    output_tyrosine = enrichment_object.kinase_enrichment(non_canonical=False,
+    enrichment_object = kl.EnrichmentData(
+        input_foreground_df, input_background_df, fg_seq_col="SITE_+/-7_AA", bg_seq_col="SITE_+/-7_AA",)
+    output_tyrosine = enrichment_object.kinase_enrichment(non_canonical=False, kin_type="tyrosine",
                                                           kl_method="percentile_rank", kl_thresh=15, kinases=['HER2', 'EGFR'])
-
     expected_df = pd.read_csv(enrichment_tyrosine_output_path, index_col=0)
     pd.testing.assert_frame_equal(output_tyrosine.enrichment_results, expected_df)
